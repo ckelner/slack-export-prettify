3babfe05d52b5eeda0c79bd8164d0755029a54d0
ckelner <chris.kelner@gmail.com>
2017-05-22 23:13:25 -0400

    First pass of parsing channels

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 /*
   Evil global vars
 */
-var zipOutput = $("#zip-output");
 var zipContent = null;
 
 /*
@@ -9,11 +8,49 @@ var zipContent = null;
 */
 function processZip(zipObject) {
   zipContent = zipObject; // set global var
+  var channelsTxt = null;
   zipContent.files["channels.json"].async('text').then(
     function success(content) {
-      zipOutput.html(content);
+      processChannels(JSON.parse(content));
     }
   );
+}
+
+/*
+  channelJSON: JSON Object of `channels.json` from the Slack Zip
+*/
+function processChannels(channelJSON) {
+  var channelHeaders = ['Name','Archived','Purpose'];
+  var channelData = [];
+  channelJSON.forEach(function(channel) {
+    channelData.push([
+      channel.name,
+      channel.is_archived,
+      channel.purpose.value
+    ]);
+  });
+  var table = $('<table></table>').addClass('table table-striped');
+  var tr = $('<tr></tr>');
+  var th = $('<th></th>');
+  var td = $('<td></td>');
+  var header = tr.clone();
+  //fill header row
+  channelHeaders.forEach(function(d) {
+    header.append(th.clone().text(d));
+  });
+  table.append($('<thead></thead>').append(header));
+  var tbody = $('<tbody></tbody>');
+  //fill out the table body
+  channelData.forEach(function(d) {
+    var row = tr.clone();
+    d.forEach(function(e, j) {
+      row.append(td.clone().text(e));
+    });
+    tbody.append(row);
+  });
+  table.append(tbody);
+  $("#welcome").hide();
+  $("#zip-output").append(table);
 }
 
 /*
