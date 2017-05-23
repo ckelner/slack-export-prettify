080430f456cf2bd3ef696c958341bffd801c2e9b
ckelner <chris.kelner@gmail.com>
2017-05-22 22:41:10 -0400

    Slightly more modular code
    Codeback hell

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
+/*
+  Evil global vars
+*/
+var zipOutput = $("#zip-output");
+var zipContent = null;
+
+/*
+  zipObject: the JSZip ZipObject from the slack zip upload
+*/
+function processZip(zipObject) {
+  zipContent = zipObject; // set global var
+  zipContent.files["channels.json"].async('text').then(
+    function success(content) {
+      zipOutput.html(content);
+    }
+  );
+}
+
+/*
+  Adds onchange watcher to file input form, loads the zip, and kicks off
+  processing the zip
+*/
 (function(obj) {
   (function() {
     var fileInput = document.getElementById("file-input");
-    var zipOutput = $("#zip-output");
     fileInput.addEventListener('change', function() {
-      JSZip.loadAsync(fileInput.files[0]).then(function(content) {
-        return content.files["channels.json"].async('text');
-      }).then(function (txt) {
-         zipOutput.html(txt);
+      JSZip.loadAsync(fileInput.files[0]).then(function(promise) {
+        return promise;
+      }).then(function onFulfill(content) {
+        processZip(content);
       });
     }, false);
   })();
